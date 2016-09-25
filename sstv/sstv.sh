93f45271209567e8558a6f1710295c9659abb275
Richard Neese <r.neese@gmail.com>
2016-09-25 11:44:59 -0500

    renaming dir and files

--- sstv/sstv.sh
+++ sstv/sstv.sh
@@ -3,7 +3,7 @@ txt=this is my text
 
 echo "Capture Image "
 sudo raspistill -w 800 -h 600 -o /home/pi/SSTV/sstv.jpg
-echo "Add custom txext to img "
+echo "Add custom text to img "
 sudo convert /home/pi/SSTV/sstv.jpg -gravity south -stroke "#000C" -strokewidth 2 -annotate 0 "$txt" -stroke none -fill white -annotate 0 "$txt" /home/pi/SSTV/sstv.jpg
 echo " compilation sstv "
 sudo python /home/pi/PySSTV-0.2.7/pysstv --vox /home/pi/SSTV/sstv.jpg /home/pi/SSTV/sstv.wav
